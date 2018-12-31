1e3212cd929c8843cc5a482efd664256f56f3064
kenjikatooo <k.524.10nis@gmail.com>
2018-12-31 18:21:07 +0900

    👍  update knock setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
-  
+  # resources :posts
+  jsonapi_resources :posts
+
 end
