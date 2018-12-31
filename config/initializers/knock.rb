47299c26137c2a801e7bfd2a14321c41945ab84a
kenjikatooo <k.524.10nis@gmail.com>
2018-12-31 19:53:20 +0900

    🎉  fix knock setting

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -36,7 +36,7 @@ Knock.setup do |config|
   ## Configure the key used to sign tokens.
   ##
   ## Default:
-  config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  config.token_secret_signature_key = -> { Rails.application.credentials.secret_key_base }
   # こっちでも同じことっぽい
   # config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
 
